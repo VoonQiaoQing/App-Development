cf94e63513f0bf26ff95bd341a02a14cb048f0b5
Voon Qiao Qing <77107489+VoonQiaoQing@users.noreply.github.com>
2021-01-14 21:58:25 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,18 +1,45 @@
-from flask import Flask,request,render_template,request, redirect, url_for, session
+from flask import send_from_directory, Flask, flash,render_template,request, redirect, url_for, session
 from Forms import CreateUserForm
-from EditMovies import CreateMoviesForm
+from EditMovies import UpdateMoviesForm
 from EditRooms import CreateRoomsForm
-import shelve, MoviesStaff
+import shelve, RoomInfo, MovieInfo, HomeInfo
+from werkzeug.utils import secure_filename
+import os
+
+path = os.getcwd()
+UPLOAD_FOLDER = os.path.join(path, 'uploads')
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+# Make directory if "uploads" folder not exists
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['SECRET_KEY'] = 'thisisasecret'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/Movies')
-def movies():
-    return render_template('movies.html')
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+#@app.route('/Movies')
+#def movies():
+#    staff_dict = {}
+#    db = shelve.open('storage.db', 'r')
+#    staff_dict = db['Staff']
+#    db.close()
+
+#    staff_list = []
+#    for key in staff_dict:
+#        staff = staff_dict.get(key)
+#        staff_list.append(staff)
+
+#    return render_template('movies.html', staff_list=staff_list)
 
 @app.route('/filter')
 def vmd_timestamp():
@@ -22,75 +49,197 @@ def vmd_timestamp():
 def purchase():
     return render_template('moviesstaff.html')
 
+#moviesstaff - retrieve page
+#staff_list – The 'staff_list list' will be used in the Retrieve Users template
+# to retrieve and display the details of all the user objects that
+# were stored in the users_dict dictionary that was stored in shelve.
 @app.route('/MoviesStaff')
 def moviesstaff():
-    return render_template('moviesstaff.html')
+    movieinfo_dict = {}
+    db = shelve.open('movieinfostorage.db', 'r')
+    movieinfo_dict = db['MovieInfo']
+    db.close()
 
-@app.route('/staffeditrooms', methods=['GET', 'POST'])
-def staffeditrooms():
-    create_staff_roomform = CreateRoomsForm(request.form)
-    if request.method == 'POST' and create_staff_roomform.validate():
+    movieinfo_list = []
+    for key in movieinfo_dict:
+        movieinfo = movieinfo_dict.get(key)
+        movieinfo_list.append(movieinfo)
 
-        staff_dict = {}
-        db = shelve.open('storage.db', 'w')
-        db['Staff'] = staff_dict
+    return render_template('moviesstaff.html', movieinfo_list=movieinfo_list)
 
-#        staff = MoviesStaff.MoviesStaff(create_staff_roomform.room_title.data,
-#                                        create_staff_roomform.small_roominfo.data,
-#                                        create_staff_roomform.med_roominfo.data,
-#                                        create_staff_roomform.large_roominfo.data,
-#                                        )
+#@app.route('/MoviesStaff')
+#def moviesstaff():
+#    movieinfo_dict = {}
+#    db = shelve.open('movieinfostorage.db', 'r')
+#    movieinfo_dict = db['MovieInfo']
+#    db.close()
 
-        staff = staff_dict.get(id)
-        staff.set_roomtitle(create_staff_roomform.room_title.data)
-        staff.set_small_roominfo(create_staff_roomform.small_roominfo.data)
-        staff.set_med_roominfo(create_staff_roomform.med_roominfo.data)
-        staff.set_large_roominfo(create_staff_roomform.large_roominfo.data)
+#    movieinfo_list = []
+#    for key in movieinfo_dict:
+#        movieinfo = movieinfo_dict.get(key)
+#        movieinfo_list.append(movieinfo)
 
-#        staff_dict[staff.get_staff_id()] = staff
-        db['Staff'] = staff_dict
+#    return render_template('moviesstaff.html', movieinfo_list=movieinfo_list)
+
+@app.route('/ChooseMovies')
+def ChooseMovies():
+    movieinfo_dict = {}
+    db = shelve.open('movieinfostorage.db', 'r')
+    movieinfo_dict = db['MovieInfo']
+    db.close()
+
+    movieinfo_list = []
+    for key in movieinfo_dict:
+        movieinfo = movieinfo_dict.get(key)
+        movieinfo_list.append(movieinfo)
+
+    return render_template('chooseMovies.html', movieinfo_list=movieinfo_list)
+
+#@app.route('/CreateMovies', methods=['GET', 'POST'])
+#def upload_file():
+#    if request.method == 'POST':
+        # check if the post request has the file part
+#        if 'file' not in request.files:
+#            flash('No file part')
+#            return redirect(request.url)
+#            file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+#        if file.filename == '':
+#            flash('No selected file')
+#            return redirect(request.url)
+#        if file and allowed_file(file.filename):
+#            filename = secure_filename(file.filename)
+#            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#           flash('File successfully uploaded')
+#            return redirect (url_for('staffeditmovies',filename=filename))
+#        else:
+#            flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+#            return redirect(request.url)
+
+@app.route('/CreateMovies', methods=['GET', 'POST'])
+def CreateMovies(): #(filename?)
+    create_movieform = UpdateMoviesForm(request.form)
+#    file_url = send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+    if request.method == 'POST' and create_movieform.validate():
+
+        movieinfo_dict = {}
+        db = shelve.open('movieinfostorage.db', 'c')
+
+        try:
+            movieinfo_dict = db['MovieInfo']
+        except:
+            print("Error in retrieving Staff from storage.db.")
+
+        movieinfo = MovieInfo.MovieInfo(
+#                                   create_movieform.movie_title.data,
+                                    create_movieform.movie_name.data,
+#                                   file_url,
+                                    create_movieform.movieagerating.data,
+                                    create_movieform.movieduration.data,
+                                    create_movieform.gvexclusivetag.data,
+                                    create_movieform.movieHorror.data,
+                                    create_movieform.movieDrama.data,
+                                    create_movieform.movieComedy.data,
+                                    create_movieform.movieScience.data,
+                                    create_movieform.movieRomance.data,
+                                    create_movieform.movieAnimation.data,
+                                    create_movieform.movieCrimeFilm.data,
+                                    create_movieform.movieThriller.data,
+                                    create_movieform.movieAdventure.data,
+                                    create_movieform.movieEmotional.data,
+                                    create_movieform.movieMystery.data,
+                                    create_movieform.movieAction.data)
+
+        movieinfo_dict[movieinfo.get_movie_id()] = movieinfo
+        db['MovieInfo'] = movieinfo_dict
 
         # Test codes
-#        staff_dict = db['Staff']
-#        staff = staff_dict[staff.get_staff_id()]
-#        print("was stored in storage.db successfully with user_id ==", staff.get_user_id())
+        movieinfo_dict = db['MovieInfo'] #Value of Staff Key in Storage
+        movieinfo = movieinfo_dict[movieinfo.get_movie_id()] #value of Value of Staff Key in Storage
+        print("was stored in storage.mb successfully with user_id ==", movieinfo.get_movie_id())
 
         db.close()
-
         return redirect(url_for('home'))
+    return render_template('createMovie.html', form=create_movieform)
+
+@app.route('/UpdateMovie/<int:id>/', methods=['GET', 'POST'])
+def UpdateMovie(id):
+    update_movieform = UpdateMoviesForm(request.form)
+    if request.method == 'POST' and update_movieform.validate():
+        movieinfo_dict = {}
+        db = shelve.open('movieinfostorage.db', 'w')
+        movieinfo_dict = db['MovieInfo']
+
+        movie = movieinfo_dict.get(id)
+#        movie.set_movietitle(update_movieform.movie_title.data)
+        movie.set_moviename(update_movieform.movie_name.data)
+#        movie.set_movie_image(update_movieform.movie_image.data)
+        movie.set_movieagerating(update_movieform.movieagerating.data)
+        movie.set_movieduration(update_movieform.movieduration.data)
+        movie.set_gvexclusivetag(update_movieform.gvexclusivetag.data)
+        movie.set_movieHorror(update_movieform.movieHorror.data)
+        movie.set_movieDrama(update_movieform.movieDrama.data)
+        movie.set_movieComedy(update_movieform.movieComedy.data)
+        movie.set_movieScience(update_movieform.movieScience.data)
+        movie.set_movieRomance(update_movieform.movieRomance.data)
+        movie.set_movieAnimation(update_movieform.movieAnimation.data)
+        movie.set_movieCrimeFilm(update_movieform.movieCrimeFilm.data)
+        movie.set_movieThriller(update_movieform.movieThriller.data)
+        movie.set_movieAdventure(update_movieform.movieAdventure.data)
+        movie.set_movieEmotional(update_movieform.movieEmotional.data)
+        movie.set_movieMystery(update_movieform.movieMystery.data)
+        movie.set_movieAction(update_movieform.movieAction.data)
+
+        db['MovieInfo'] = movieinfo_dict
+        db.close()
+
+        return redirect(url_for('moviesstaff'))
+
     else:
-        staff_dict = {}
-        db = shelve.open('storage.db', 'r')
-        db['Staff'] = staff_dict
+        movieinfo_dict = {}
+        db = shelve.open('movieinfostorage.db', 'r')
+        movieinfo_dict = db['MovieInfo']
         db.close()
 
-        staff = staff_dict.get(id)
-        create_staff_roomform.room_title.data = staff.get_roomtitle()
-        create_staff_roomform.small_roominfo.data = staff.get_small_roominfo()
-        create_staff_roomform.med_roominfo.data = staff.get_med_roominfo()
-        create_staff_roomform.large_roominfo.data = staff.get_large_roominfo()
+        movie = movieinfo_dict.get(id)
+#        update_movieform.movie_title.data = movie.get_movietitle()
+        update_movieform.movie_name.data = movie.get_moviename()
+#        update_movieform.movie_image.data = movie.get_movie_image()
+        update_movieform.movieagerating.data = movie.get_movieagerating()
+        update_movieform.movieduration.data = movie.get_movieduration()
+        update_movieform.gvexclusivetag.data = movie.get_gvexclusivetag()
+        update_movieform.movieHorror.data = movie.get_movieHorror()
+        update_movieform.movieDrama.data = movie.get_movieDrama()
+        update_movieform.movieComedy.data = movie.get_movieComedy()
+        update_movieform.movieScience.data = movie.get_movieScience()
+        update_movieform.movieRomance.data = movie.get_movieRomance()
+        update_movieform.movieAnimation.data = movie.get_movieAnimation()
+        update_movieform.movieCrimeFilm.data = movie.get_movieCrimeFilm()
+        update_movieform.movieThriller.data = movie.get_movieThriller()
+        update_movieform.movieAdventure.data = movie.get_movieAdventure()
+        update_movieform.movieEmotional.data = movie.get_movieEmotional()
+        update_movieform.movieMystery.data = movie.get_movieMystery()
+        update_movieform.movieAction.data = movie.get_movieAction()
 
-        staff_list = []
-        for key in staff_dict:
-            staff = staff_dict.get(key)
-            staff_list.append(staff)
+        return render_template('updateMovie.html', form=update_movieform)
 
-        return render_template('editRooms.html', form=create_staff_roomform, staff_list=staff_list)
+#@app.route('/delete_user', methods=['POST'])
+#def delete_movie():
+#    staff_dict = {}
+#    db = shelve.open('storage.db', 'w')
+#    staff_dict = db['Staff']
 
-@app.route('/staffeditmovies', methods=['GET', 'POST'])
-def staffeditmovies():
-    create_user_form2 = CreateMoviesForm(request.form)
-    if request.method == 'POST' and create_user_form2.validate():
-        return redirect(url_for('home'))
-    return render_template('editMovies.html', form=create_user_form2)
+#    staff_dict.pop()
 
-@app.route('/contactUs')
-def contact_us():
-    return render_template('contactUs.html')
+#    db['Staff'] = staff_dict
+#    db.close()
 
-@app.route('/Staff')
-def staff():
-    return render_template('testform.html')
+#    return redirect(url_for('moviesstaff.html'))
+
+#@app.route('/Staff')
+#def staff():
+#    return render_template('testform.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
