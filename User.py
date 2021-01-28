e8d1821655f09f134b212a5ba26a878c7c502415
SaransFaceLol <77654678+SaransFaceLol@users.noreply.github.com>
2021-01-28 23:04:15 +0800

    Add files via upload

--- User.py
+++ User.py
@@ -1,26 +1,37 @@
 class User:
     count_id = 0
 
-    def __init__(self, rating, review):
-        User.count_id += 1
+    def __init__(self,Question,Answer,Availability,NumberOfPeople,remarks):
+        User.count_id +=1
         self.__user_id = User.count_id
-        self.__rating = rating
-        self.__review = review
+        self.__Question= Question
+        self.__Answer= Answer
+        self.__Availability= Availability
+        self.__NumberOfPeople= NumberOfPeople
+        self.__remarks= remarks
 
-    def set_user_id(self, user_id):
-        self.__user_id = user_id
-
-    def set_rating(self, rating):
-        self.__rating = rating
-
-    def set_review(self, review):
-        self.__review = review
+    def set_user_id(self,user_id):
+        self.__user_id=user_id
+    def set_Question(self,Question):
+        self.__Question=Question
+    def set_Answer(self,Answer):
+        self.__Answer=Answer
+    def set_Availability(self,Availability):
+        self.__Availability=Availability
+    def set_NumberOfPeople(self,NumberOfPeople):
+        self.__NumberOfPeople=NumberOfPeople
+    def set_remarks(self,remarks):
+        self.__remarks=remarks
 
     def get_user_id(self):
         return self.__user_id
-
-    def get_rating(self):
-        return self.__rating
-
-    def get_review(self):
-        return self.__review
+    def get_Question(self):
+        return self.__Question
+    def get_Answer(self):
+        return self.__Answer
+    def get_Availability(self):
+        return self.__Availability
+    def get_NumberOfPeople(self):
+        return self.__NumberOfPeople
+    def get_remarks(self):
+        return self.__remarks
