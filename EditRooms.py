efdcf2e88a5f678276fa37bc9029802ab2bb9cad
Voon Qiao Qing <77107489+VoonQiaoQing@users.noreply.github.com>
2021-02-02 01:39:55 +0800

    Update EditRooms.py

--- EditRooms.py
+++ EditRooms.py
@@ -1,20 +1,19 @@
-from wtforms import Form, FileField, RadioField, SelectField, TextAreaField, validators
+from flask_uploads import UploadSet, IMAGES
+from wtforms import Form, TextAreaField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileField, FileRequired
+
+photos = UploadSet('photos', IMAGES)
 
 class CreateRoomsForm(Form):
-    room_title = TextAreaField('Rooms Pricings Title', [validators.DataRequired()])
-    small_roominfo = TextAreaField('Small Room Information', [validators.DataRequired()])
-#    small_roomimage =
-    med_roominfo = TextAreaField('Medium Room Information', [validators.DataRequired()])
-#    med_roomimage1 =
-#    med_roomimage2 =
-    large_roominfo = TextAreaField('Large Room Information', [validators.DataRequired()])
-#    large_roomimage1 =
-#    large_roomimage2 =
-    gvexclusiveinfo =  TextAreaField('GV Exclusive Information', [validators.DataRequired()])
-#Form object – Used to create a WTForms Form.
-#StringField object – Used to create an HTML textfield.
-#RadioField object – Used to create an HTML radio button group.
-#SelectField object – Used to create an HTML dropdown list.
-#TextAreaField object – Used to create an HTML textarea.
-#validators object – validators allow you to specify the constraints for each of the fields such as minimum and maximum length, DataRequried or Optional.
+    room_title = TextAreaField('Rooms Pricings Title', validators=[DataRequired()])
+    small_roominfo = TextAreaField('Small Room Information', validators=[DataRequired()])
+    small_roomimage1 = FileField(validators=[FileRequired()])
+    small_roomimage2 = FileField(validators=[FileRequired()])
+    med_roominfo = TextAreaField('Medium Room Information', validators=[DataRequired()])
+    med_roomimage = FileField(validators=[FileRequired()])
+    large_roominfo = TextAreaField('Large Room Information', validators=[DataRequired()])
+    large_roomimage1 = FileField(validators=[FileRequired()])
+    large_roomimage2 = FileField(validators=[FileRequired()])
+    gvexclusiveinfo =  TextAreaField('GV Exclusive Information', validators=[DataRequired()])
 
