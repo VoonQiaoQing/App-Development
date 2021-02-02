8583d58ff743e79783b80d7b328f37aff3b853be
SaransFaceLol <77654678+SaransFaceLol@users.noreply.github.com>
2021-02-02 11:59:37 +0800

    Add files via upload

---
+++ NigelForms.py
@@ -0,0 +1,8 @@
+from wtforms import Form, StringField, RadioField, SelectField, TextAreaField, validators
+
+class CreateUserForm(Form):
+    Question = StringField('Question', [validators.Length(min=1, max=150), validators.DataRequired()])
+    Answer = StringField('Answer', [validators.Length(min=1, max=150), validators.DataRequired()])
+    Availability = SelectField('Does the Chatbot have the answer?', [validators.DataRequired()], choices=[('', 'Select'), ('Yes', 'Yes'), ('No', 'No')], default='')
+    NumberOfPeople= RadioField('Number of people who suggested this', choices=[('1', '1 to 10'), ('2', '11 to 20'), ('3', '21 to 30'),("4","Above 30 ")], default='1')
+    remarks = TextAreaField('Remarks', [validators.Optional()])
