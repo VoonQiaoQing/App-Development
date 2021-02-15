0570229e3cfbfa7222591d9ed4fbf9c597002f22
Voon Qiao Qing <77107489+VoonQiaoQing@users.noreply.github.com>
2021-02-15 23:21:19 +0800

    Update __init__.py

--- QiaoQing/__init__.py
+++ QiaoQing/__init__.py
@@ -8,8 +8,13 @@ from EditHomeImage import EditHomeImageForm
 from EditHomeDescription import EditHomeDescriptionForm
 from EditMovies import UpdateMoviesForm, photos
 from EditRooms import CreateRoomsForm
-from Forms import CreateUserForm,LoginForm, RegisterForm
-import shelve, RoomInfo, MovieInfo, UserId, User, HomeDescription, HomeImage, NewestReleases
+from Forms import CreateUserForm,LoginForm, RegisterForm, Purchase, Add_time, Add_location, Roomtype
+from checkoutform import Checkout
+import MovieInfoToTicket as MITT
+import admin as admin
+from pathlib import Path
+
+import shelve, RoomInfo, MovieInfo, UserId, User, HomeDescription, HomeImage, NewestReleases, payment, booking, random, string
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import CombinedMultiDict
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -28,6 +33,481 @@ login_manager.login_view = 'login'
 
 configure_uploads(app, photos)
 patch_request_class(app)
+
+@app.route('/add_location', methods=['GET', 'POST'])
+def add_location():
+    add_location_form = Add_location(request.form)
+    if request.method == 'POST' and add_location_form.validate():
+        add_location_dict = {}
+        db = shelve.open('storage.db', 'c')
+        try:
+            add_location_dict = db['Location']
+        except:
+            print('Error in retrieving form from admin_location.db')
+
+        add_location = admin.Add_location(add_location_form.location1.data, add_location_form.location2.data,  add_location_form.location3.data, add_location_form.location4.data, add_location_form.location5.data)
+        add_location_dict[add_location.get_location_Id()] = add_location
+        db['Location'] = add_location_dict
+        db.close()
+        return redirect(url_for('retrieve_location'))
+    return render_template('admin_location.html',form=add_location_form)
+@app.route('/retrieve_location')
+def retrieve_location():
+    add_location_dict = {}
+    db = shelve.open('storage.db','r' )
+    add_location_dict = db['Location']
+    db.close()
+
+    add_location_list = []
+    for key in add_location_dict:
+        admin = add_location_dict.get(key)
+        add_location_list.append(admin)
+    return render_template('retrieve_location.html', count=len(add_location_list), add_location_list=add_location_list)
+
+@app.route('/updatelocation/<int:id>/', methods=['GET', 'POST'])
+def updatelocation(id):
+    update_location_form = Add_location(request.form)
+    if request.method == 'POST' and update_location_form.validate():
+        location_dict = {}
+        db = shelve.open('storage.db', 'w')
+        location_dict = db['Location']
+        location = location_dict.get(id)
+        location.set_location1(update_location_form.location1.data)
+        location.set_location2(update_location_form.location2.data)
+        location.set_location3(update_location_form.location3.data)
+        location.set_location4(update_location_form.location4.data)
+        location.set_location5(update_location_form.location5.data)
+        db['Location'] = location_dict
+        db.close()
+        return redirect(url_for('retrieve_location'))
+    else:
+        location_dict = {}
+        db = shelve.open('storage.db', 'r')
+        location_dict = db['Location']
+        db.close()
+        location = location_dict.get(id)
+        update_location_form.location1.data = location.get_location1()
+        update_location_form.location2.data = location.get_location2()
+        update_location_form.location3.data = location.get_location3()
+        update_location_form.location4.data = location.get_location4()
+        update_location_form.location5.data = location.get_location5()
+        return render_template('updatelocation.html', form=update_location_form)
+
+@app.route('/add_time', methods=['GET', 'POST'])
+def admin_time():
+    add_time_form = Add_time(request.form)
+    if request.method == 'POST' and add_time_form.validate():
+        add_time_dict = {}
+        db = shelve.open('storage.db', 'c')
+        try:
+            add_time_dict = db['Time']
+        except:
+            print('Error in retrieving form from admin_time.db')
+
+        add_time = admin.Add_time(add_time_form.time1.data, add_time_form.time2.data,add_time_form.time3.data,add_time_form.time4.data,add_time_form.time5.data)
+        add_time_dict[add_time.get_time_id()] = add_time
+        db['Time'] = add_time_dict
+        db.close()
+        return redirect(url_for('retrieve_time'))
+    return render_template('admin_time.html',form=add_time_form)
+@app.route('/retrieve_time')
+def retrieve_time():
+    add_time_dict = {}
+    db = shelve.open('storage.db','r' )
+    add_time_dict = db['Time']
+    db.close()
+    add_time_list = []
+    for key in add_time_dict:
+        admin = add_time_dict.get(key)
+        add_time_list.append(admin)
+    return render_template('retrieve_time.html', count=len(add_time_list), add_time_list=add_time_list)
+@app.route('/updatetime/<int:id>/', methods=['GET', 'POST'])
+def updatetime(id):
+    update_time_form = Add_time(request.form)
+    if request.method == 'POST' and update_time_form.validate():
+        time_dict = {}
+        db = shelve.open('storage.db', 'w')
+        time_dict = db['Time']
+        time = time_dict.get(id)
+        time.set_time1(update_time_form.time1.data)
+        time.set_time2(update_time_form.time2.data)
+        time.set_time3(update_time_form.time3.data)
+        time.set_time4(update_time_form.time4.data)
+        time.set_time5(update_time_form.time5.data)
+        db['Time'] = time_dict
+        db.close()
+        return redirect(url_for('retrieve_time'))
+    else:
+        time_dict = {}
+        db = shelve.open('storage.db', 'r')
+        time_dict = db['Time']
+        db.close()
+        time = time_dict.get(id)
+        update_time_form.time1.data = time.get_time1()
+        update_time_form.time2.data = time.get_time2()
+        update_time_form.time3.data = time.get_time3()
+        update_time_form.time4.data = time.get_time4()
+        update_time_form.time5.data = time.get_time5()
+        return render_template('updatetime.html', form=update_time_form)
+
+@app.route('/Purchase/<int:movieid>/', methods=['GET', 'POST'])
+def purchase(movieid):
+    purchase_form = Purchase(request.form)
+
+    movieinfo_dict = {}
+    db = shelve.open('movieinfostorage.db', 'r')
+    movieinfo_dict = db['MovieInfo']
+    movieinfo = movieinfo_dict.get(movieid)
+    db.close()
+
+    admin_location_dict = {}
+    db = shelve.open('storage.db', 'r')
+    admin_location_dict = db['Location']
+    db.close()
+
+    admin_location_list = []
+    for key in admin_location_dict:
+        admin = admin_location_dict.get(key)
+        admin_location_list.append(admin)
+
+    for location in admin_location_list:
+        purchase_form.location.choices = [location.get_location1(),location.get_location2(),location.get_location3(),location.get_location4(),location.get_location5()]
+
+    admin_time_dict = {}
+    db = shelve.open('storage.db','r' )
+    admin_time_dict = db['Time']
+    db.close()
+
+    admin_time_list = []
+    for key in admin_time_dict:
+        admin = admin_time_dict.get(key)
+        admin_time_list.append(admin)
+
+    for time in admin_time_list:
+        purchase_form.Time.choices =[time.get_time1(),time.get_time2(),time.get_time3(),time.get_time4(),time.get_time5()]
+
+    if request.method == 'POST' and purchase_form.validate():
+        purchase_dict = {}
+        db = shelve.open('storage.db', 'c')
+        try:
+            purchase_dict = db['Purchase']
+        except:
+            print("Error in retrieving form from purchase.db.")
+
+        purchase = booking.Booking(purchase_form.Date.data
+                                   ,purchase_form.Time.data
+                                   ,purchase_form.location.data)
+
+        if len(purchase_dict) == 0:
+            currentpurchaseid = 1
+        else: #1:movieinfo
+            last = list(movieinfo_dict.keys())[-1]
+            currentpurchaseid = last + 1
+
+        purchase.set_BookDateTimeLocationID(currentpurchaseid)
+        purchaseid = purchase.get_BookDateTimeLocationID()
+        purchase_dict[purchase.get_BookDateTimeLocationID()] = purchase
+        db['Purchase'] = purchase_dict
+        db.close()
+
+        return redirect(url_for('roomtype', purchaseid=purchaseid, movieid=movieid))
+    return render_template('purchase.html', form=purchase_form, movieinfo=movieinfo)
+
+@app.route('/Createsroomtype')
+def Createroomtype():
+
+    bookingleft_dict = {}
+    db = shelve.open('storage.db', 'c')
+    try:
+        bookingleft_dict = db["BookingLeft"]
+    except:
+        print("Error in retrieving storage.db.")
+
+    room16Jan = booking.Left("16Jan")
+    room17Jan = booking.Left("17Jan")
+    room18Jan = booking.Left("18Jan")
+    room19Jan = booking.Left("19Jan")
+    room20Jan = booking.Left("20Jan")
+
+    bookingleft_dict[room16Jan.get_LeftID()] = room16Jan
+    bookingleft_dict[room17Jan.get_LeftID()] = room17Jan
+    bookingleft_dict[room18Jan.get_LeftID()] = room18Jan
+    bookingleft_dict[room19Jan.get_LeftID()] = room19Jan
+    bookingleft_dict[room20Jan.get_LeftID()] = room20Jan
+
+    db["BookingLeft"] = bookingleft_dict
+
+    # Test codes
+    bookingleft_dict = db["BookingLeft"] #Value of Staff Key in Storage
+    room20Jan = bookingleft_dict[room16Jan.get_LeftID()] #value of Value of Staff Key in Storage
+    print("was stored in storage.mb successfully with user_id ==", room20Jan.get_LeftID())
+
+    db.close()
+
+@app.route('/roomtype/<int:movieid>/<int:purchaseid>/', methods=['GET', 'POST'])
+def roomtype(movieid,purchaseid):
+    roomtype_form = Roomtype(request.form)
+
+    movieinfo_dict = {}
+    db = shelve.open('movieinfostorage.db', 'r')
+    movieinfo_dict = db['MovieInfo']
+    db.close()
+
+    movieinfo = movieinfo_dict.get(movieid) #specific object
+
+    purchase_dict = {}
+    db = shelve.open('storage.db', 'r')
+    purchase_dict = db['Purchase']
+    db.close()
+
+    purchaseinfo = purchase_dict.get(purchaseid)
+
+    moviename = movieinfo.get_moviename()
+    location = purchaseinfo.get_location()
+    time = purchaseinfo.get_Time()
+    date = purchaseinfo.get_Date()
+
+    if request.method == 'POST' and roomtype_form.validate():
+        roomtype_dict = {}
+        room = shelve.open('storage.db', 'c')
+
+        try:
+            roomtype_dict = db['Roomtype']
+        except:
+            print("Error in retrieving form from storage.db.")
+
+        if len(roomtype_dict) == 0:
+            new = 1
+        else:
+            last = list(roomtype_dict.keys())[-1]
+            new = last + 1
+
+        lower_upper_alphabet = string.ascii_letters
+        l = random.choice(lower_upper_alphabet)
+        l2 = random.choice(lower_upper_alphabet)
+        ref = "3"+l+"2"+l2
+
+        type = booking.Roomtype(new,
+                                ref,
+                                roomtype_form.roomtype.data,
+                                moviename,
+                                date,
+                                time,
+                                location)
+
+        roomtypeid = type.get_num()
+        roomtype_dict[roomtypeid] = type
+        room['Roomtype'] = roomtype_dict
+        room.close()
+
+        roomtype_dict = {}
+        db = shelve.open('storage.db', 'r')
+        roomtype_dict = db['Roomtype']
+        db.close()
+
+        roomtypeinfo = roomtype_dict.get(roomtypeid) #specific object
+
+        return redirect(url_for('checkout', roomtypeinfo=roomtypeinfo, movieinfo=movieinfo, purchaseinfo=purchaseinfo, purchaseid=purchaseid, movieid=movieid, roomtypeid=roomtypeid))
+
+    roomtype_dict = {}
+    db = shelve.open('storage.db','r')
+    roomtype_dict = db['Roomtype']
+    db.close()
+
+    roomtype_list = []
+    for key in roomtype_dict:
+        room = roomtype_dict.get(key)
+        roomtype_list.append(room)
+
+    purchase_dict = {}
+    db = shelve.open('storage.db', 'r')
+    purchase_dict = db['Purchase']
+    db.close()
+
+    purchase_list = []
+    for key in purchase_dict:
+        purchase = purchase_dict.get(key)
+        purchase_list.append(purchase)
+
+    GetDate = purchaseinfo.get_Date()
+    DateGotten = 0
+
+    if GetDate == "16/01/2021":
+        DateGotten = "16Jan"
+
+    if GetDate == "17/01/2021":
+        DateGotten = "17Jan"
+
+    if GetDate == "18/01/2021":
+        DateGotten = "18Jan"
+
+    if GetDate == "19/01/2021":
+        DateGotten = "19Jan"
+
+    if GetDate == "20/01/2021":
+        DateGotten = "20Jan"
+
+    bookingleft_dict = {}
+    db = shelve.open('storage.db', 'r')
+    bookingleft_dict = db["BookingLeft"]
+    db.close()
+
+    bookingleft_list = []
+    for key in bookingleft_dict:
+        bookingleft = bookingleft_dict.get(key)
+        bookingleft_list.append(bookingleft)
+
+    for k in roomtype_list:
+        for i in purchase_list:
+            if i.get_location() == k.get_location() \
+                    and i.get_Time() == k.get_Time() \
+                    and i.get_Date() == k.get_Date():
+
+                bookingleft_dict = {}
+                db = shelve.open('storage.db', 'w')
+                bookingleft_dict = db["BookingLeft"]
+                db.close()
+
+                existingbooking = bookingleft_list[DateGotten]
+                small_left = existingbooking.get_small_left()
+                med_left = existingbooking.get_medium_left()
+                big_left = existingbooking.get_big_left()
+
+                if k.get_roomtype() == 'Small':
+                    small_left += 1
+                existingbooking.set_small_left(small_left)
+
+                if k.get_roomtype() == 'Medium':
+                    med_left += 1
+                existingbooking.set_medium_left(med_left)
+
+                if k.get_roomtype() == 'Big':
+                    big_left += 1
+                existingbooking.set_big_left(big_left)
+
+    return render_template('roomtype.html'
+                           ,roomtype_list = roomtype_list
+                           , purchase_list=purchase_list
+                            , movieid = movieid
+                            , purchaseid = purchaseid
+                           ,room=room, form=roomtype_form)
+
+@app.route('/checkout', methods=['GET', 'POST'])
+def checkout():
+    purchase_dict = {}
+    db = shelve.open('storage.db', 'r')
+    purchase_dict = db['Purchase']
+    db.close()
+    roomtype_dict = {}
+    db = shelve.open('storage.db','r')
+    roomtype_dict = db['Roomtype']
+    db.close()
+    room_list = []
+    for key in roomtype_dict:
+        room = roomtype_dict.get(key)
+        room_list.append(room)
+    for i in room_list:
+        if i == room_list[-1]:
+            room_type = i.get_roomtype()
+        #     roomtype_list = []
+        #     for key in roomtype_dict:
+        #         roomtype = roomtype_dict.get(key)
+        # purchase_list.append(purchase)
+    checkout_form = Checkout(request.form)
+    if request.method == 'POST' and checkout_form.validate():
+        checkout_dict = {}
+        db = shelve.open('storage.db', 'c')
+        try:
+            checkout_dict = db['Checkout']
+        except:
+            print("Error in retrieving form from checkout.db .")
+        checkout = payment.Payment(checkout_form.card_number1.data,checkout_form.name_on_card.data,
+                                   checkout_form.expiry_month.data, checkout_form.expiry_year.data,
+                                   checkout_form.cvv_number.data)
+        checkout_dict[checkout.get_cvv_number()] = checkout
+        db['Checkout'] = checkout_dict
+        db.close()
+        return redirect(url_for('booked'))
+    return render_template('checkout.html',room_type=room_type, form=checkout_form, room=room_list)
+
+@app.route('/booked')
+def booked():
+    purchase_dict = {}
+    db = shelve.open('storage.db', 'r')
+    purchase_dict = db['Purchase']
+    db.close()
+    purchase_list = []
+    for key in purchase_dict:
+        purchase = purchase_dict.get(key)
+        purchase_list.append(purchase)
+    roomtype_dict = {}
+    db = shelve.open('storage.db','r')
+    roomtype_dict = db['Roomtype']
+    db.close()
+    roomtype_list = []
+    for key in roomtype_dict:
+        roomtype = roomtype_dict.get(key)
+        roomtype_list.append(roomtype)
+    booked_dict = {}
+    db = shelve.open('storage.db', 'c')
+    try:
+        booked_dict = db['Booked']
+    except:
+        print("Error in retrieving form from booked.db .")
+    for i in purchase_list:
+        for k in roomtype_list:
+            if i.get_location() == k.get_location() and i.get_Time() == k.get_Time() and i.get_Date() == k.get_Date():
+                last = 0
+                if len(booked_dict) == 0:
+                    new = 1
+                else:
+                    last = list(booked_dict.keys())[-1]
+                    new = last + 1
+                booked = booking.Booked(new,k.get_ref(),k.get_moviename(),k.get_Date(),k.get_Time(),k.get_location(),k.get_roomtype())
+                booked_dict[booked.get_customerid()] = booked
+                db['Booked'] = booked_dict
+                db.close()
+    return redirect(url_for('success'))
+@app.route('/success')
+def success():
+    booked_dict = {}
+    db = shelve.open('storage.db','r' )
+    booked_dict = db['Booked']
+    db.close()
+    booked_list = []
+    for key in booked_dict:
+        ticket = booked_dict.get(key)
+        booked_list.append(ticket)
+    for i in booked_list:
+        if i == booked_list[-1]:
+            movie = i.get_moviename()
+    return render_template('success.html',movie=movie, booked_list=booked_list)
+
+@app.route('/ticket')
+def ticket():
+    booked_dict = {}
+    db = shelve.open('storage.db','r' )
+    booked_dict = db['Booked']
+    db.close()
+    booked_list = []
+    for key in booked_dict:
+        ticket = booked_dict.get(key)
+        booked_list.append(ticket)
+    return render_template('Tickets.html', count=len(booked_list), purchase_list=booked_list)
+@app.route('/view/<int:id>/', methods=['GET', 'POST'])
+def view(id):
+        booked_dict = {}
+        db = shelve.open('storage.db', 'r')
+        booked_dict = db['Booked']
+        db.close()
+        booked = booked_dict.get(id)
+        movie = booked.get_moviename()
+        Date = booked.get_Date()
+        Time = booked.get_Time()
+        Location = booked.get_location()
+        roomtype = booked.get_roomtype()
+        ref = booked.get_ref()
+        return render_template('view.html',ref=ref,movie=movie, booked=booked,Date=Date,Time=Time,location=Location,roomtype=roomtype)
 
 class CuUser(UserMixin):
     id = 0
@@ -754,6 +1234,11 @@ def moviesfilter():
     print(image_list)
     return render_template('moviesfilter.html', movieinfo_list=movieinfo_list,  roominfo_list=roominfo_list, image_list=image_list)
 
+@app.route('/Review/<userid>')
+def Review(userid=None):
+    session['login'] = userid
+    return render_template('contactUs.html',userid=userid)
+
 @app.route('/contactUs/None/')
 @app.route('/contactUs/<userid>')
 def contact_us(userid=None):
@@ -840,22 +1325,6 @@ def delete_user(id):
         db.close()
 
         return redirect(url_for('retrieve_questions'))
-
-@app.route('/Purchase/<userid>/<int:movieid>/')
-def Purchase(userid,movieid):
-
-    movieinfo_dict = {}
-    db = shelve.open('movieinfostorage.db', 'r')
-    movieinfo_dict = db['MovieInfo']
-    db.close()
-
-    movieinfo_list = []
-    movieinfo = movieinfo_dict.get(movieid)
-    movieinfo_list.append(movieinfo)
-
-    session['login'] = userid
-
-    return render_template('Purchase.html',userid=userid, movieinfo_list=movieinfo_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
