737a7b111002962640a4353e8ae933e247b06aac
Voon Qiao Qing <77107489+VoonQiaoQing@users.noreply.github.com>
2021-02-15 13:12:22 +0800

    Update EditRooms.py

--- QiaoQing/EditRooms.py
+++ QiaoQing/EditRooms.py
@@ -1,5 +1,5 @@
 from flask_uploads import UploadSet, IMAGES
-from wtforms import Form, TextAreaField
+from wtforms import Form, TextAreaField, IntegerField
 from wtforms.validators import DataRequired
 from flask_wtf.file import FileField, FileRequired
 
@@ -8,11 +8,20 @@ photos = UploadSet('photos', IMAGES)
 class CreateRoomsForm(Form):
     room_title = TextAreaField('Rooms Pricings Title', validators=[DataRequired()])
     small_roominfo = TextAreaField('Small Room Information', validators=[DataRequired()])
+    small_roomprice = IntegerField('Small Room Price',render_kw={"placeholder": "00"})
     small_roomimage1 = FileField(validators=[FileRequired()])
     small_roomimage2 = FileField(validators=[FileRequired()])
     med_roominfo = TextAreaField('Medium Room Information', validators=[DataRequired()])
+    med_roomprice = IntegerField('Medium Room Price',render_kw={"placeholder": "00"})
     med_roomimage = FileField(validators=[FileRequired()])
     large_roominfo = TextAreaField('Large Room Information', validators=[DataRequired()])
+    large_roomprice = IntegerField('Large Room Price',render_kw={"placeholder": "00"})
     large_roomimage1 = FileField(validators=[FileRequired()])
     large_roomimage2 = FileField(validators=[FileRequired()])
-    gvexclusiveinfo =  TextAreaField('GV Exclusive Information', validators=[DataRequired()])
+
+    gvexclusivesmall_roominfo =  TextAreaField('GV Exclusive Small Room Information', validators=[DataRequired()])
+    gvexclusivesmall_roomprice = IntegerField('GV Exclusive Small Room Price',render_kw={"placeholder": "00"})
+    gvexclusivemed_roominfo =  TextAreaField('GV Exclusive Medium Room Information', validators=[DataRequired()])
+    gvexclusivemed_roomprice = IntegerField('GV Exclusive Medium Room Price',render_kw={"placeholder": "00"})
+    gvexclusivelarge_roominfo = TextAreaField('GV Exclusive Large Room Information', validators=[DataRequired()])
+    gvexclusivelargeroomprice =  IntegerField('GV Exclusive Large Room Price',render_kw={"placeholder": "00"})
